38ea1412fe2b2ba10caca0a5c374041ce6d44c05
Maicon Anderson Mattos da Silva <maiconmattos@Maicons-MacBook-Pro.local>
2017-05-19 16:06:15 +0100

    Added code to test data insertion

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ MongoClient.connect(url, function(err, db) {
     console.log("Connnected successfully to server");
     db.close
 });
+
+var insertDocuments = function(db, callback) {
+    var collection = db.collection('documents');
+    collection.insertMany(
+        [{a : 1}, {a : 2}, {a : 3}],
+        function(err, result) {
+            assert.equal(err, null);
+            assert.equal(3 , result.result.n);
+            assert.equal(3, result.ops.length);
+            console.log("Inserted 3 documents into the collection");
+            callback(result);
+        }
+    );
+}
+
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    insertDocuments(db, function() {
+        db.close;
+    })
+});
+      
